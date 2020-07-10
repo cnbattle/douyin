8bf12aa30b16b73020d33f12ef2b1c5c8f83830a
cnbattle <qiaicn@gmail.com>
2020-07-10 21:09:16 +0800

    fix core.go Convert Int to String Error

--- internal/core/core.go
+++ internal/core/core.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/cnbattle/douyin/internal/config"
 	"github.com/cnbattle/douyin/internal/database"
@@ -41,8 +42,8 @@ func HandleJson(data model.Data) {
 		video.Nickname = item.Author.Nickname
 		video.Avatar = localAvatar
 		video.Desc = item.Desc
-		video.DiggCount = string(item.Statistics.DiggCount)
-		video.CommentCount = string(item.Statistics.CommentCount)
+		video.DiggCount = strconv.Itoa(item.Statistics.DiggCount)
+		video.CommentCount = strconv.Itoa(item.Statistics.CommentCount)
 		video.ShareUrl = item.ShareInfo.ShareUrl
 		video.CoverPath = localCover
 		video.VideoPath = localVideo
